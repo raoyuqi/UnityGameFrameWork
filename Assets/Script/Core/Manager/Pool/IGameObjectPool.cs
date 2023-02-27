42e8c3a08ebadc1fba8389901e4c32ebf8253172
unknown <xinzheng@diezhi.local>
2023-02-27 16:39:28 +0800

    feat:默认对象池

---
+++ Assets/Script/Core/Manager/Pool/IGameObjectPool.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace FrameWork.Core.Manager
+{
+    public interface IGameObjectPool
+    {
+        int Size { get; set; }
+
+        // 从对象池取出物体
+        GameObject GetGameObject();
+
+        // 归还对象池
+        void RecycleObject(GameObject gameObject);
+
+        // 销毁物体，且不归还
+        void DestroyGameObject(GameObject gameObject);
+
+        // 清空对象池
+        void Clean();
+    }
+}
