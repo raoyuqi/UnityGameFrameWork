a4296f515bb0846ef73571d3abc7c39516267b1d
raoyuqi <rao_yuqi@163.com>
2023-03-01 22:59:29 +0800

    feat:UI事件系统模板

---
+++ Assets/Script/Core/Modules/Signal/ISignalSystem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FrameWork.Core.Modules.Signal
+{
+    public delegate void CallBack(object sender, params object[] args);
+
+    public interface ISignalSystem
+    {
+        // 发信号
+        void RaisedSignal();
+
+        // 注册信号
+        void RegisterSignal(Enum name, CallBack callBack);
+
+        //void RegisterSignal(string signalName, CallBack callBack);
+
+        // 移除单个信号
+        void RemoveSignal(Enum name, CallBack callBack);
+
+        //void RemoveSignal(string signalName, CallBack callBack);
+
+        // 移除指定类型的所有信号
+        void RemoveAllSignal(Enum name);
+
+        //void RemoveAllSignal(string signalName);
+    }
+}
---
+++ Assets/Script/Core/Modules/Signal/UISignalSystem.cs
@@ -0,0 +1,47 @@
+﻿using FrameWork.Core.Modules.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FrameWork.Core.Modules.Signal
+{
+    public enum UISignal
+    {
+        OnOpened = 1,
+        OnClosed = 2,
+        OnHide = 3,
+        OnShow = 4,
+        OnInit = 5,
+        OnRefresh = 6,
+        OnDestroy = 7
+    }
+
+    public delegate void UICallBack(UIPanelBase panel, params object[] args);
+
+    public sealed class UISignalSystem
+    {
+        public void RaisedSignal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterSignal(UISignal name, UICallBack callBack)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAllSignal(UISignal name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveSignal(UISignal name, UICallBack callBack)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    // 传参基类，TODO：转移到全局事件系统
+    public class SignalArgs { }
+}
